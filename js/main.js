282a7ecbc29be96d63ee8bd5512a71761073f073
natchapon <natchapon.f@gmail.com>
2013-12-17 02:05:58 +0700

    playable

--- js/main.js
+++ js/main.js
@@ -1,50 +1,63 @@
 
-
+var testSS;
 
 $(function() {
 	// create an new instance of a pixi stage
-	var stage = new PIXI.Stage(0x66FF99);
+	var stage = new PIXI.Stage(0x66FF99,true);
 
 	// create a renderer instance.
-	var renderer = PIXI.autoDetectRenderer(512, 384);
+	var renderer = PIXI.autoDetectRenderer(810, 724);
 
 	// add the renderer view element to the DOM
 	//document.body.appendChild(renderer.view);
 	$("#canvas").append(renderer.view);
 
-	// create a texture from an image path
-	var texture = PIXI.Texture.fromImage("images/icon.jpg");
-	// create a new Sprite using the texture
-	var bunny = new PIXI.Sprite(texture);
-	
-	
-	scroller = new Scroller(stage);
+//	// create a texture from an image path
+//	var texture = PIXI.Texture.fromImage("images/icon.jpg");
+//	// create a new Sprite using the texture
+//	var bunny = new PIXI.Sprite(texture);
+//
+//
+//	var scroller = new Scroller(stage);
+//
+//
+//	// center the sprites anchor point
+//	bunny.anchor.x = 0.5;
+//	bunny.anchor.y = 0.5;
+//
+//	// move the sprite t the center of the screen
+//	bunny.position.x = 200;
+//	bunny.position.y = 150;
+//
+//	stage.addChild(bunny);
+    var assetsToLoader = [ "images/pack.json"];
 
-	
-	// center the sprites anchor point
-	bunny.anchor.x = 0.5;
-	bunny.anchor.y = 0.5;
+    // create a new loader
+    var loader = new PIXI.AssetLoader(assetsToLoader);
 
-	// move the sprite t the center of the screen
-	bunny.position.x = 200;
-	bunny.position.y = 150;
+    // use callback
+    loader.onComplete = onAssetsLoaded;
 
-	stage.addChild(bunny);
-	
-	
-	requestAnimFrame(loop);
+
+    //begin load
+    loader.load();
+    var scroller;
+    function onAssetsLoaded(){
+
+        scroller = new Scroller(stage);
+
+        testSS = scroller;
+        requestAnimFrame(loop);
+
+    }
 
 	function loop() {
 
 		requestAnimFrame(loop);
 		
 		
-		scroller.setViewportX( scroller.viewportX + 3);
-		
-		
-		// just for fun, lets rotate mr rabbit a little
-		bunny.rotation -= 0.1;
-		bunny.alpha = (bunny.alpha + 1 ) % 100;
+		scroller.update( );
+
 
 		// render the stage
 		renderer.render(stage);
--- js/scroller.js
+++ js/scroller.js
@@ -1,38 +1,154 @@
 
-function Scroller(stage){
-	var farTexture = PIXI.Texture.fromImage("images/bg-far.png");
-	this.far = new TileBg(farTexture, 512 , 256);
-	this.far.deltaX = 0.128;
-	stage.addChild(this.far);
-	
+Scroller.prototype = Object.create( PIXI.DisplayObjectContainer.prototype );
+Scroller.prototype.constructor = Scroller;
 
-	var midTexture = PIXI.Texture.fromImage("images/bg-mid.png");
-	this.mid = new TileBg(midTexture, 512 , 256);
-	this.mid.deltaX = 0.64;
-	this.mid.position.y = 128;
-	
+function Scroller(stage){
+    PIXI.DisplayObjectContainer.call(this);
+//    var midTexture = PIXI.Texture.fromFrame("actor.png");
+//
+//	this.mid = new TileBg(midTexture, 460 , 530);
+//	this.mid.deltaX = 0.64;
+//	this.mid.position.x = 0;
+//
+//	this.window = new PIXI.Sprite.fromFrame("window.png")
+//
+//    this.windowClose = new PIXI.Sprite.fromFrame("window_close.png");
+//
 	this.viewportX = 0;
-	
-	stage.addChild(this.mid); 
+    this.rowList = [];
+    this.index = 0;
+    this.position.x = 100;
+
+    for (var i=0; i < 5; i++){
+
+        var row = new SeatRow(this);
+        row.position.x =  i * 480;
+        this.rowList.push(row);
+
+        this.addChild(row);
+    }
+
+    stage.addChild(this);
 }
 
-Scroller.prototype.update = function() {
-  this.far.tilePosition.x -= 0.128;
-  this.mid.tilePosition.x -= 0.64;
-};
+Scroller.prototype.next = function(){
+    this.index++;
+    console.log(this);
+}
+
+Scroller.prototype.update = function(){
+    var offset = -100;
+    if(this.position.x + offset > -(this.index * 480 )){
+        this.position.x -= 5;
+    }
+}
 
 Scroller.prototype.setViewportX = function(x) {
-  this.far.setViewportX(x);
-  this.mid.setViewportX(x);
+  //this.far.setViewportX(x);
+  //this.mid.setViewportX(x);
   this.viewportX = x;
 };
 
 
 
+SeatRow.prototype = Object.create( PIXI.DisplayObjectContainer.prototype );
+SeatRow.prototype.constructor = SeatRow;
 
+function SeatRow(scroller){
+
+    PIXI.DisplayObjectContainer.call(this);
+
+    this.bg = PIXI.Sprite.fromFrame("actor.png");
+    this.windowClose = PIXI.Sprite.fromFrame("window_close.png");
+    this.windowClose.position.x = 5;
+    this.windowClose.position.y = 80;
+    this.windowClose.oriHeight = this.windowClose.height;
+
+    this.window = PIXI.Sprite.fromFrame("window.png");
+    this.window.position.x = 5;
+    this.window.position.y = 80;
+
+    this.addChild(this.bg);
+    this.addChild(this.window);
+    this.addChild(this.windowClose);
+
+
+    this.windowClose.interactive = true;
+    this.windowClose.buttonMode = true;
+
+    this.windowClose.mousedown = this.windowClose.touchstart = function(data)
+    {
+        // stop the default event...
+        data.originalEvent.preventDefault();
+
+        // store a refference to the data
+        // The reason for this is because of multitouch
+        // we want to track the movement of this particular touch
+        this.data = data;
+        this.alpha = 0.9;
+        this.dragging = true;
+
+        this.startPos = this.data.global.y;
+
+
+        console.log(data);
+    };
+
+    // set the events for when the mouse is released or a touch is released
+    this.windowClose.mouseout = this.windowClose.mouseup = this.windowClose.mouseupoutside = this.windowClose.touchend = this.windowClose.touchendoutside = function(data)
+    {
+        this.alpha = 1
+        this.dragging = false;
+        // set the interaction data to null
+        this.data = null;
+
+    };
+
+    // set the callbacks for when the mouse or a touch moves
+    this.windowClose.mousemove = this.windowClose.touchmove = function(data)
+    {
+        if(this.dragging)
+        {
+            // need to get parent coords..
+            //var newPosition = this.data.getLocalPosition(this.parent);
+
+            var diff = this.startPos - this.data.global.y;
+
+            if( diff > this.oriHeight * 0.9 ){
+
+                diff = this.oriHeight * 0.9;
+
+                score();
+            }
+
+            if( diff < 0 ){
+                diff = 0;
+            }
+            this.height = this.oriHeight - (diff * 0.9);
+        }
+    }
+
+    var scored = false;
+
+    function score(){
+        if( !scored ){
+            scored = true;
+            setTimeout( function(){scroller.next() }, 1000)
+
+            //scroller.next();
+
+            console.log("whee");
+        }
+    }
+}
+
+
+
+// Tile Bg Class
 function TileBg(texture, width, height) {
   PIXI.TilingSprite.call(this, texture, width, height);
-	
+  this.width = width;
+  this.height = height;
   this.position.x = 0;
   this.position.y = 0;
   this.tilePosition.x = 0;
